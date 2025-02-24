65e4547d74fa1eac91c3bee081fda4f84004cc27
Warpcode <warpcodeuk@gmail.com>
2025-02-24 18:09:27 +0000

    Update vagrant to run on openSSH

---
+++ scripts/ProvisionWithOpenSSH.ps1
@@ -0,0 +1,25 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force;
+
+# If it's not present, install it
+winget install "openssh beta"
+
+$firewallParams = @{
+    Name        = 'sshd-Server-In-TCP'
+    DisplayName = 'Inbound rule for OpenSSH Server (sshd) on TCP port 22'
+    Action      = 'Allow'
+    Direction   = 'Inbound'
+    Enabled     = 'True'  # This is not a boolean but an enum
+    Profile     = 'Any'
+    Protocol    = 'TCP'
+    LocalPort   = 22
+}
+New-NetFirewallRule @firewallParams
+
+$shellParams = @{
+    Path         = 'HKLM:\SOFTWARE\OpenSSH'
+    Name         = 'DefaultShell'
+    Value        = 'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe'
+    PropertyType = 'String'
+    Force        = $true
+}
+New-ItemProperty @shellParams
